f2bbf9741651507f65f57217cb4d165adc5b785f
Ali <>
2021-12-24 19:19:11 +0500

    fourth commit

--- app/src/main/java/com/example/sqlite_project/DbHelper.java
+++ app/src/main/java/com/example/sqlite_project/DbHelper.java
@@ -1,8 +1,12 @@
 package com.example.sqlite_project;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 import androidx.annotation.Nullable;
 
@@ -29,5 +33,31 @@ public class DbHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + STUDENT_TABLE);
         onCreate(sqLiteDatabase);
     }
-    
+
+    public void  addStudent(Student_M Student_M){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(STUDENT_NAME, Student_M.getName());
+        cv.put(STUDENT_AGE, Student_M.getAge());
+        cv.put(ACTIVE_STUDENT, Student_M.isActive());
+        db.insert(STUDENT_TABLE, null, cv);
+        db.close();
+
+    }
+
+    public ArrayList<Student_M> getAllStudents() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursorCourses = db.rawQuery("SELECT * FROM " + STUDENT_TABLE, null);
+        ArrayList<Student_M> studentArrayList = new ArrayList<>();
+        if (cursorCourses.moveToFirst()) {
+            do {
+                studentArrayList.add(new Student_M(cursorCourses.getString(1),
+                        cursorCourses.getInt(2),
+                        cursorCourses.getInt(3) == 1 ? true : false));
+            } while (cursorCourses.moveToNext());
+
+        }
+        cursorCourses.close();
+        return studentArrayList;
+    }
 }
--- app/src/main/java/com/example/sqlite_project/MainActivity.java
+++ app/src/main/java/com/example/sqlite_project/MainActivity.java
@@ -3,12 +3,57 @@ package com.example.sqlite_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonAdd, buttonViewAll;
+    EditText editName, editAge;
+    Switch switchIsActive;
+    ListView listViewStudent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonViewAll = findViewById(R.id.buttonViewAll);
+        editName = findViewById(R.id.editTextName);
+        editAge = findViewById(R.id.editTextAge);
+        switchIsActive = findViewById(R.id.switchStudent);
+        listViewStudent = findViewById(R.id.listViewStudent);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            Student_M Student_M;
+            @Override
+            public void onClick(View v) {
+                try {
+                    Student_M = new Student_M(editName.getText().toString(), Integer.parseInt(editAge.getText().toString()), switchIsActive.isChecked());
+                    Toast.makeText(MainActivity.this, Student_M.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                DbHelper dbHelper = new DbHelper(MainActivity.this);
+                dbHelper.addStudent(Student_M);
+            }
+        });
+
+        buttonViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DbHelper dbHelper = new DbHelper(MainActivity.this);
+                List<Student_M> list = dbHelper.getAllStudents();
+                ArrayAdapter arrayAdapter = new ArrayAdapter<Student_M>(MainActivity.this, android.R.layout.simple_list_item_1,list);
+                listViewStudent.setAdapter(arrayAdapter);
+
+            }
+        });
     }
 }
