a58cf3198a1c2e6d093a62be87aab2241594486a
Ali <>
2021-12-24 16:12:41 +0500

    third com

--- app/src/main/java/com/example/sqlite_project/DbHelper.java
+++ app/src/main/java/com/example/sqlite_project/DbHelper.java
@@ -1,18 +1,33 @@
 package com.example.sqlite_project;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DbHelper extends SQLiteOpenHelper {
+import androidx.annotation.Nullable;
 
+public class DbHelper extends SQLiteOpenHelper {
+    public static final String STUDENT_NAME = "STUDENTName";
+    public static final String STUDENT_AGE = "STUDENTAge";
+    public static final String ACTIVE_STUDENT = "ActiveSTUDENT";
+    public static final String STUDENT_ID = "STUDENTID";
+    public static final String STUDENT_TABLE = "StudentTable";
+
+
+    public DbHelper(@Nullable Context context) {
+        super(context,"studentDB.db", null, 4);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String createTableSTatement = "CREATE TABLE " + STUDENT_TABLE + "(" + STUDENT_ID + " Integer PRIMARY KEY AUTOINCREMENT, " + STUDENT_NAME + " Text, " + STUDENT_AGE + " Int, " + ACTIVE_STUDENT + " BOOL) ";
+        sqLiteDatabase.execSQL(createTableSTatement);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + STUDENT_TABLE);
+        onCreate(sqLiteDatabase);
     }
+    
 }
