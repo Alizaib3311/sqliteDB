3cefd4b19e4a9c85e3dcdbb45135911d8d9c5f8d
Ali <>
2021-12-24 15:39:31 +0500

    second

---
+++ app/src/main/java/com/example/sqlite_project/DbHelper.java
@@ -0,0 +1,18 @@
+package com.example.sqlite_project;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
